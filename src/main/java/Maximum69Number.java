868c0d8b50d4fd84285296020c61625f7674878a
dasha-buzovska <buzovskadaryna@gmail.com>
2020-03-24 10:11:24 +0200

    1323. Maximum 69 Number

---
+++ src/main/java/Maximum69Number.java
@@ -0,0 +1,14 @@
+public class Maximum69Number {
+
+    public int maximum69Number(int num) {
+        char[] chars = Integer.toString(num).toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(chars));
+    }
+
+}
