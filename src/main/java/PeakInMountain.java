39132a09633c429c883e14c741d7197aeeb6cfa6
dasha-buzovska <buzovskadaryna@gmail.com>
2020-04-23 11:10:39 +0300

    852. Peak Index in a Mountain Array

---
+++ src/main/java/PeakInMountain.java
@@ -0,0 +1,13 @@
+public class PeakInMountain {
+    public int peakIndexInMountainArray(int[] A) {
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
